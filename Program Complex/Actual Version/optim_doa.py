341535881240a29c1366244dffebd5374fe2f44c
Recognizer525 <grsurzhikov@yandex.ru>
2025-12-28 04:24:29 +0300

    Some improvement of theta estimation was performed

--- Program Complex/Actual Version/optim_doa.py
+++ Program Complex/Actual Version/optim_doa.py
@@ -35,11 +35,11 @@ def cost_theta(theta: torch.Tensor,
     return torch.norm(E, 'fro')**2  # скалярный тензор
 
 
-def find_angles_start(Sigma_XS_np: np.ndarray, 
+def find_angles_start(Sigma_XS: torch.Tensor, 
                       theta0_np: np.ndarray, 
-                      P_np: np.ndarray, 
-                      Q_inv_sqrt_np: np.ndarray,
-                      bounds: object = None, 
+                      P: torch.Tensor, 
+                      Q_inv_sqrt: torch.Tensor,
+                      bounds: list|None = None, 
                       method: str = 'SLSQP', 
                       tol: int = 1e-6) -> tuple[np.ndarray, float]:
     """
@@ -47,16 +47,16 @@ def find_angles_start(Sigma_XS_np: np.ndarray,
 
     Parameters
     ---------------------------------------------------------------------------
-    Sigma_XS_np: np.ndarray
+    Sigma_XS: torch.Tensor
         Кросс-ковариация наблюдений и сигналов с текущего Е-шага.
     theta0_np: np.ndarray
         Первое приближение для оценивания DoA. 
         Представляет собой одномерный массив.
-    P_np: torch.Tensor
+    P: torch.Tensor
         Оценка ковариации сигналов с предыдущей итерации.
-    Q_inv_sqrt_np: np.ndarray
+    Q_inv_sqrt: torch.Tensor
         Квадратный корень от матрицы, обратной к ковариационной матрице шума. 
-    bounds: object
+    bounds: list|None
         Границы, в пределах которых надо искать оптимальное значение вектора DoA.
     method: str = 'SLSQP'
         Метод оптимизации функции потерь для DoA.
@@ -73,24 +73,21 @@ def find_angles_start(Sigma_XS_np: np.ndarray,
     """
     if bounds is None:
         bounds = [(-np.pi/2, np.pi/2)] * len(theta0_np)
-    def fun(theta_np: np.ndarray) -> tuple[float, np.ndarray]:
+    def fun_and_grad(theta_np: np.ndarray) -> tuple[float, np.ndarray]:
         """
         Возвращает значение функции потерь и значение градиента.
         """
         theta_t = torch.tensor(theta_np, 
-                               dtype=torch.float32, 
+                               dtype=torch.float64, 
                                requires_grad=True)
-        Sigma_XS_t = torch.tensor(Sigma_XS_np, dtype=torch.cfloat)
-        P_t = torch.tensor(P_np, dtype=torch.cfloat)
-        Q_inv_sqrt_t = torch.tensor(Q_inv_sqrt_np, dtype=torch.cfloat)
         
-        loss = cost_theta(theta_t, Sigma_XS_t, P_t, Q_inv_sqrt_t)
+        loss = cost_theta(theta_t, Sigma_XS, P, Q_inv_sqrt)
         loss.backward()
-        grad = theta_t.grad.detach().numpy().astype(np.float64)
+        grad = theta_t.grad.detach().numpy()
         return loss.item(), grad
 
-    res = minimize(lambda th: fun(th)[0], theta0_np, 
-                   jac=lambda th: fun(th)[1], bounds=bounds, method=method, tol=tol)
+    res = minimize(fun_and_grad, theta0_np, 
+                   jac=True, bounds=bounds, method=method, tol=tol)
     #print(f"Optim.res={res.success}")
     return res.x, res.fun
 
@@ -100,7 +97,7 @@ def find_angles(Sigma_XS_np: np.ndarray,
                 P_np: np.ndarray, 
                 Q_inv_sqrt_np: np.ndarray, 
                 num_of_starts: int = 7,
-                bounds: object = None) -> np.ndarray:
+                bounds: list|None = None) -> np.ndarray:
     """
     Функция предназначена для поиска оценки DoA, которая минимизирует норму
     ||Q^{-1/2}(Sigma_XS-AP)||^2_F.
@@ -119,7 +116,7 @@ def find_angles(Sigma_XS_np: np.ndarray,
     num_of_starts: int
         Число начальных приближений относительно которых нужно проводить
         оптимизационный процесс.
-    bounds: object
+    bounds: list|None
         Границы, в пределах которых надо искать оптимальное значение вектора DoA.
 
     Returns
@@ -128,18 +125,24 @@ def find_angles(Sigma_XS_np: np.ndarray,
         Полученная в ходе процесса оптимизации наилучшая оценка вектора DoA.
     """
     best_theta, best_fun = None, np.inf
+
+    Sigma_XS_t = torch.tensor(Sigma_XS_np, dtype=torch.complex128)
+    P_t = torch.tensor(P_np, dtype=torch.complex128)
+    Q_inv_sqrt_t = torch.tensor(Q_inv_sqrt_np, dtype=torch.complex128)
+
+
     for i in range(num_of_starts):
         if i == 0:
-            est_theta, est_fun = find_angles_start(Sigma_XS_np, theta0_np, 
-                                                   P_np, Q_inv_sqrt_np,
+            est_theta, est_fun = find_angles_start(Sigma_XS_t, theta0_np, 
+                                                   P_t, Q_inv_sqrt_t,
                                                    bounds=bounds)
         else:
             K = len(theta0_np)
             nu = np.random.RandomState(42+i).uniform(-np.pi, np.pi)
             theta = np.array([(nu + j * 2 * np.pi/K) % (2 * np.pi) 
                               for j in range(K)]) - np.pi
-            est_theta, est_fun = find_angles_start(Sigma_XS_np, theta, 
-                                                   P_np, Q_inv_sqrt_np,
+            est_theta, est_fun = find_angles_start(Sigma_XS_t, theta, 
+                                                   P_t, Q_inv_sqrt_t,
                                                    bounds=bounds)
         if est_fun < best_fun:
             best_fun, best_theta = est_fun, est_theta
--- Program Complex/Actual Version/optim_doa_custom.py
+++ Program Complex/Actual Version/optim_doa_custom.py
@@ -38,12 +38,12 @@ def cost_angles(angles: torch.Tensor,
     return torch.norm(E, 'fro')**2  # скалярный тензор
 
 
-def find_angles_start(Sigma_XS_np: np.ndarray, 
+def find_angles_start(Sigma_XS: torch.Tensor, 
                       angles0_np: np.ndarray,
-                      coords_np: np.ndarray, 
-                      P_np: np.ndarray, 
-                      Q_inv_sqrt_np: np.ndarray,
-                      bounds: object = None, 
+                      coords: torch.Tensor, 
+                      P: torch.Tensor, 
+                      Q_inv_sqrt: torch.Tensor,
+                      bounds: list|None = None, 
                       method: str = 'SLSQP', 
                       tol: int = 1e-6) -> tuple[np.ndarray, float]:
     """
@@ -51,18 +51,18 @@ def find_angles_start(Sigma_XS_np: np.ndarray,
 
     Parameters
     ---------------------------------------------------------------------------
-    Sigma_XS_np: np.ndarray
+    Sigma_XS: torch.Tensor
         Кросс-ковариация наблюдений и сигналов на текущем Е-шаге.
     angles0_np: np.ndarray
         Первое приближение для оценивания DoA. 
         Представляет собой одномерный массив.
-    coords_np: np.ndarray
+    coords: torch.Tensor
         Координаты сенсоров, массив размера (L,3).
-    P_np: np.ndarray
+    P: torch.Tensor
         Оценка ковариации сигналов с предыдущей итерации.
-    Q_inv_sqrt_np: np.ndarray
+    Q_inv_sqrt: torch.Tensor
         Квадратный корень от матрицы, обратной к ковариационной матрице шума. 
-    bounds: object
+    bounds: list|None
         Границы, в пределах которых надо искать оптимальное значение вектора DoA.
     method: str = 'SLSQP'
         Метод оптимизации функции потерь для DoA.
@@ -84,14 +84,10 @@ def find_angles_start(Sigma_XS_np: np.ndarray,
         angles_t = torch.tensor(angles_np, 
                                dtype=torch.float32, 
                                requires_grad=True)
-        coords_t = torch.tensor(coords_np, dtype=torch.float32)
-        Sigma_XS_t = torch.tensor(Sigma_XS_np, dtype=torch.cfloat)
-        P_t = torch.tensor(P_np, dtype=torch.cfloat)
-        Q_inv_sqrt_t = torch.tensor(Q_inv_sqrt_np, dtype=torch.cfloat)
         
-        loss = cost_angles(angles_t, Sigma_XS_t, P_t, coords_t, Q_inv_sqrt_t)
+        loss = cost_angles(angles_t, Sigma_XS, P, coords, Q_inv_sqrt)
         loss.backward()
-        grad = angles_t.grad.detach().numpy().astype(np.float64)
+        grad = angles_t.grad.detach().numpy()
         return loss.item(), grad
 
     res = minimize(lambda th: fun(th)[0], angles0_np, 
@@ -106,8 +102,7 @@ def find_angles(Sigma_XS_np: np.ndarray,
                 P_np: np.ndarray, 
                 Q_inv_sqrt_np: np.ndarray, 
                 num_of_starts: int = 7,
-                bounds: object = None,
-                method: str = 'SLSQP') -> np.ndarray:
+                bounds: list|None = None) -> np.ndarray:
     """
     Функция предназначена для поиска оценки DoA, которая минимизирует норму
     ||Q^{-1/2}(Sigma_XS-AP)||^2_F.
@@ -128,7 +123,7 @@ def find_angles(Sigma_XS_np: np.ndarray,
     num_of_starts: int
         Число начальных приближений относительно которых нужно проводить
         оптимизационный процесс.
-    bounds: object
+    bounds: list|None
         Границы, в пределах которых надо искать оптимальное значение вектора DoA.
     method: str
         Метод оптимизации функции потерь для DoA.
@@ -139,10 +134,17 @@ def find_angles(Sigma_XS_np: np.ndarray,
         Полученная в ходе процесса оптимизации наилучшая оценка вектора DoA.
     """
     best_angles, best_fun = None, np.inf
+
+    Sigma_XS_t = torch.tensor(Sigma_XS_np, dtype=torch.complex128)
+    P_t = torch.tensor(P_np, dtype=torch.complex128)
+    Q_inv_sqrt_t = torch.tensor(Q_inv_sqrt_np, dtype=torch.complex128)
+    coords_t = torch.tensor(coords_np, dtype=torch.float64)
+
+
     for i in range(num_of_starts):
         if i == 0:
-            est_angles, est_fun = find_angles_start(Sigma_XS_np, angles0_np,
-                                                    coords_np, P_np, Q_inv_sqrt_np,
+            est_angles, est_fun = find_angles_start(Sigma_XS_t, angles0_np,
+                                                    coords_t, P_t, Q_inv_sqrt_t,
                                                     bounds=bounds)
         else:
             K = len(angles0_np)
@@ -150,7 +152,7 @@ def find_angles(Sigma_XS_np: np.ndarray,
             angles = np.array([(nu + j * 2 * np.pi/K) % (2 * np.pi) 
                               for j in range(K)]) - np.pi
             est_angles, est_fun = find_angles_start(Sigma_XS_np, angles, 
-                                                    coords_np, P_np, Q_inv_sqrt_np,
+                                                    coords_t, P_np, Q_inv_sqrt_np,
                                                     bounds=bounds)
         if est_fun < best_fun:
             best_fun, best_angles = est_fun, est_angles
