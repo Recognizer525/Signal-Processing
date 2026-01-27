f1d130520a900b1259cb0a1e745b352fd34abc45
Recognizer525 <grsurzhikov@yandex.ru>
2026-01-27 13:09:21 +0300

    Some corrections of incomplete likelihood function

--- Program Complex for chapter I/Experimental Version/em_stoch.py
+++ Program Complex for chapter I/Experimental Version/em_stoch.py
@@ -149,7 +149,11 @@ def incomplete_lkhd(X: np.ndarray,
     """
     A = dss.A_ULA(X.shape[1], theta)
     R = A @ P @ A.conj().T + Q
-    
+
+    sgn_R, log_det_R = np.linalg.slogdet(R)
+    if sgn_R == 0:
+        raise ValueError(f"Non-inversible R")
+
     #print(f"is_psd(R)={sensors.is_psd(R)}")
     #print(f"is_psd(P)={sensors.is_psd(P)}")
     #print(f"is_psd(Q)={sensors.is_psd(Q)}")
@@ -164,12 +168,15 @@ def incomplete_lkhd(X: np.ndarray,
         if set(O[i, ]) != set(col_numbers - 1):
             O_i = O[i, ][O[i, ] > -1]
             R_o = R[np.ix_(O_i, O_i)]
+            sgn_R_o, log_det_R_o = np.linalg.slogdet(R_o)
+            if sgn_R_o == 0:
+                raise ValueError(f"Non-inversible R_o")
+
             #R_o = R_o + 1e-6 * np.eye(R_o.shape[0])
-            res += (- np.log(np.linalg.det(R_o)) - (X[i, O_i].T).conj().T @ 
+            res += (- log_det_R_o - (X[i, O_i].T).conj().T @ 
                       np.linalg.inv(R_o) @ (X[i, O_i].T))
         else:
-            res += (- np.log(np.linalg.det(R)) - 
-                    (X[i].T).conj().T @ inv_R @ (X[i].T))
+            res += (- log_det_R - (X[i].T).conj().T @ inv_R @ (X[i].T))
     return res.real
 
 
@@ -517,3 +524,58 @@ def multi_start_EM(X: np.ndarray,
             best_P, best_angles = est_P, est_angles
     print(f"best_start={best_start}")
     return best_angles, best_P, best_lhd
+
+
+def old_incomplete_lkhd(X: np.ndarray,
+                        theta: np.ndarray, 
+                        P: np.ndarray, 
+                        Q: np.ndarray) -> np.float64:
+    """
+    Вычисляет неполное правдоподобие на основании доступных наблюдений 
+    и текущей оценки параметров.
+
+    Parameters
+    ---------------------------------------------------------------------------
+    X: np.ndarray
+        Двумерный массив, соответствующий наблюдениям.
+    theta: np.ndarray
+        Одномерный массив размера (K,1). Соответствует оценке DoA.
+    P: np.ndarray
+        Оценка ковариационной матрицы исходных сигналов.
+    Q: np.ndarray
+        Ковариационная матрица шума.
+
+    Returns
+    ---------------------------------------------------------------------------
+    res: np.float64
+        Значение неполного правдоподобия.
+    """
+    A = dss.A_ULA(X.shape[1], theta)
+    R = A @ P @ A.conj().T + Q
+    
+    #print(f"is_psd(R)={sensors.is_psd(R)}")
+    #print(f"is_psd(P)={sensors.is_psd(P)}")
+    #print(f"is_psd(Q)={sensors.is_psd(Q)}")
+    #print(f"Positive P? Ans is {np.all(np.diag(P) >= 0)}")
+
+    inv_R = np.linalg.inv(R)
+    Indicator = np.isnan(X)
+    col_numbers = np.arange(1, X.shape[1]+1)
+    O = col_numbers * (Indicator == False) - 1
+    res = 0
+    for i in range(X.shape[0]):
+        if set(O[i, ]) != set(col_numbers - 1):
+            O_i = O[i, ][O[i, ] > -1]
+            R_o = R[np.ix_(O_i, O_i)]
+            #R_o = R_o + 1e-6 * np.eye(R_o.shape[0])
+            res += (- np.log(np.linalg.det(R_o)) - (X[i, O_i].T).conj().T @ 
+                      np.linalg.inv(R_o) @ (X[i, O_i].T))
+        else:
+            res += (- np.log(np.linalg.det(R)) - 
+                    (X[i].T).conj().T @ inv_R @ (X[i].T))
+    return res.real
+
+
+
+
+
